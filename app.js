25a7354b21c30e2d9b4c1c6c5bf39221bc05fdb0
CarlosPW <carlos818k@gmail.com>
2020-12-22 14:26:18 -0300

    paginas contacto y trabajos

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const toggleMenu = document.querySelector('.menu-responsive');
-const navLinks = document.querySelector('.nav-links');
-
 document.addEventListener('DOMContentLoaded',()=>{
-    
+
+    const toggleMenu = document.querySelector('.menu-responsive');
+    const navLinks = document.querySelector('.nav-links');
     
     toggleMenu.addEventListener("click", ()=> {
         navLinks.classList.toggle("menu-responsivo-activo");
