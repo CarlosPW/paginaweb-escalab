2b9c5772aeb4992c4f4e84b273e644c2364698d9
CarlosPW <carlos818k@gmail.com>
2020-12-25 14:16:50 -0300

    js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded',()=>{
 
+    const body = document.querySelector("body");
+    body.style.opacity = 1;
+    body.style.transition = "1s";
+    
+    
     const toggleMenu = document.querySelector('.menu-responsive');
     const navLinks = document.querySelector('.nav-links');
     
